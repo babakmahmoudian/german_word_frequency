5bd1e9bb6a169d9c10d94fee9c0a9cb340e15613
Babak Mahmoudian <babak.mahmoudian@gmail.com>
2026-02-14 03:23:48 +0330

    refactored and debuged the anki-frequencies generator, tested it successfully

--- configs.py
+++ configs.py
@@ -3,27 +3,27 @@
 LEMMA_BATCH_SIZE = 1000
 SPACY_MODEL = "de_core_news_lg"
 
-LEMMA_COL_TITLE = 'lemma'
-FREQUENCY_COL_TITLE = 'frequency'
-RANK_COL_TITLE = 'rank'
-WORD_COL_TITLE = 'word'
+LEMMA_COL_TITLE = "lemma"
+FREQUENCY_COL_TITLE = "frequency"
+RANK_COL_TITLE = "rank"
+WORD_COL_TITLE = "word"
 
 # List the input files and their full path to be processed
-# NOTE: All data files are assumed to follow the same format.
-INPUT_PATH = './input_data'
-INPUT_PREFIX = 'row_words_'
+# NOTE: All raw data files are assumed to follow the same format.
+INPUT_PATH = "./input_data"
+INPUT_PREFIX = "row_words_"
 WORD_COL_INDEX = 1
 FREQUENCY_COL_INDEX = 2
-SEPARATOR = '\t'
+SEPARATOR = "\t"
 
 # Specify the output file path for the processed word-frequency dictionary
 # NOTE: The output file will be overwritten if it already exists.
-OUTPUT_FILE = './output_data/all_frequencies.csv'
+OUTPUT_FILE = "./output_data/all_frequencies.csv"
 
 # Configuration for processing the Anki collection
-INPUT_ANKI = './input_data/anki_words.tsv'
-ANKI_SEPERATOR = '\t'
+INPUT_ANKI = "./input_data/anki_words.tsv"
+ANKI_SEPERATOR = "\t"
 ANKI_SKIP_ROWS = 2
 ANKI_WORD_COL_INDEX = 0
-OUTPUT_ANKI = './output_data/anki_frequencies.csv'
-OUTPUT_ANKI_SUMMARY = './output_data/anki_summary_report.txt'
+OUTPUT_ANKI = "./output_data/anki_frequencies.csv"
+OUTPUT_ANKI_SUMMARY = "./output_data/anki_summary_report.txt"
--- generate_anki_freq.py
+++ generate_anki_freq.py
@@ -1,47 +1,63 @@
-"""Compares the given Anki collection with the processed word-frequency
-dictionary and identifies missing/extra words and the words' frequencies."""
+"""Compares the given Anki collection with the processed word-frequency dictionary and
+identifies missing/extra words and the words' frequencies."""
 
-
+import logging
 import pandas as pd
 import spacy
-from tqdm import tqdm
 from configs import (
-    INPUT_ANKI, ANKI_WORD_COL_INDEX, ANKI_SKIP_ROWS, ANKI_SEPERATOR,
+    INPUT_ANKI,
+    ANKI_WORD_COL_INDEX,
+    ANKI_SKIP_ROWS,
+    ANKI_SEPERATOR,
     OUTPUT_ANKI,
-    SPACY_MODEL, LEMMA_BATCH_SIZE,
+    SPACY_MODEL,
+    LEMMA_BATCH_SIZE,
     OUTPUT_FILE,
-    LEMMA_COL_TITLE, FREQUENCY_COL_TITLE,
+    LEMMA_COL_TITLE,
+    FREQUENCY_COL_TITLE,
+    WORD_COL_TITLE,
+    RANK_COL_TITLE,
 )
 
-print("Reading the input data files...")
-anki_df = pd.read_csv(INPUT_ANKI,
-                      usecols=[ANKI_WORD_COL_INDEX],
-                      sep=ANKI_SEPERATOR,
-                      skiprows=ANKI_SKIP_ROWS,
-                      header=None,
-                      index_col=0)
-frequency_df = pd.read_csv(OUTPUT_FILE, index_col=0)
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
-print("Loading the spaCy model for lemmatization...")
+logging.info("Starting Anki frequency generation process.")
+
+logging.info("Reading Anki collection from: %s", INPUT_ANKI)
+anki_df = pd.read_csv(
+    INPUT_ANKI,
+    usecols=[ANKI_WORD_COL_INDEX],
+    sep=ANKI_SEPERATOR,
+    skiprows=ANKI_SKIP_ROWS,
+    header=None,
+    names=[WORD_COL_TITLE],
+    index_col=None,
+)
+
+logging.info("Reading frequency data from: %s", OUTPUT_FILE)
+frequency_df = pd.read_csv(
+    OUTPUT_FILE,
+    index_col=None,
+    header=0,
+    usecols=[LEMMA_COL_TITLE, FREQUENCY_COL_TITLE, RANK_COL_TITLE],
+)
+
+logging.info("Loading spaCy model: %s", SPACY_MODEL)
 nlp = spacy.load(SPACY_MODEL)
 
-print("Lemmatizing Anki words...")
-anki_lemmas = []
-for doc in tqdm(nlp.pipe(anki_df.index, batch_size=LEMMA_BATCH_SIZE),
-                total=len(anki_df),
-                desc="Lemmatizing words"):
-    anki_lemmas.append(doc[-1].lemma_)
-anki_df[LEMMA_COL_TITLE] = anki_lemmas
+logging.info("Lemmatizing Anki words")
+anki_df[LEMMA_COL_TITLE] = [doc[-1].lemma_ for doc in nlp.pipe(anki_df[WORD_COL_TITLE], batch_size=LEMMA_BATCH_SIZE)]
 
-print("Merging the Anki words with their frequencies...")
-anki_df = anki_df.merge(frequency_df, left_on=LEMMA_COL_TITLE,
-                        right_index=True, how='left')
+logging.info("Merging Anki words with frequency data")
+anki_df = anki_df.merge(frequency_df, left_on=LEMMA_COL_TITLE, right_on=LEMMA_COL_TITLE, how="left")
 
 anki_df[FREQUENCY_COL_TITLE] = anki_df[FREQUENCY_COL_TITLE].fillna(0).astype(int)
+anki_df[RANK_COL_TITLE] = anki_df[RANK_COL_TITLE].fillna(0).astype(int)
 
-print("Sorting the Anki words by frequency...")
-anki_df = anki_df.sort_values(by=FREQUENCY_COL_TITLE, ascending=False, na_position='last')
+logging.info("Sorting the Anki words by frequency")
+anki_df = anki_df.sort_values(by=FREQUENCY_COL_TITLE, ascending=False, na_position="last")
 
-print("Saving the Anki words with their frequencies to a file...")
-anki_df.to_csv(OUTPUT_ANKI, header=[LEMMA_COL_TITLE, FREQUENCY_COL_TITLE])
-print("Anki frequencies saved to:", OUTPUT_ANKI)
+logging.info("Saving Anki frequencies to: %s", OUTPUT_ANKI)
+anki_df[[WORD_COL_TITLE, LEMMA_COL_TITLE, FREQUENCY_COL_TITLE, RANK_COL_TITLE]].to_csv(OUTPUT_ANKI, index=False)
+
+logging.info("Anki frequency generation process completed.")
